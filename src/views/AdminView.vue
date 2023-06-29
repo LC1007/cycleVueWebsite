6d6e91e01b5d16ad40f2174ff2b691736e581d53
LC1007 <tonyg7212@gmail.com>
2023-06-29 14:21:46 +0200

    changes

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,6 +1,43 @@
 <template>
     <div class="container">
         <h1 class="text-center m-5">Admin Page</h1>
+        <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary mb-4" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    Add Bike
+  </button>
+  
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Add Bike</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Bike Name</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1">
+              </div>
+              <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Bike Price</label>
+                <input type="number" class="form-control" id="exampleFormControlInput1">
+              </div>
+              <div class="mb-5">
+                <label for="exampleFormControlTextarea1" class="form-label">Description</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+              </div>
+              <div class="d-flex justify-content-center">
+                <button class="btn btn-primary">Send</button>
+              </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
         <table class="table">
             <thead>
                 <tr>
